Added fixed vs floating point numerical comparisons using RMSE

derivative_unit.py
@@ -3,14 +3,14 @@ from hardware_unit import HardwareUnit
 
 class DerivativeUnit(HardwareUnit):
 
-  def __init__(self, name: str, window_size: int, vector_width: int):
+  def __init__(self, name: str, window_size: int, vector_width: int, is_fixed_point: bool):
     # 1. FIR Stage: (1/8) * [2x[n] + x[n-1] - x[n-3] - 2x[n-4]]
     #    - Exploit DLP: We emulate SIMD vectorization where "vector_width" samples are processed in parallel.
     #    - Cost: 3 ops (add, sub, sub) per sample. (1/8)* is a "free" bit-shift.
     fir_cycles = (3 * window_size) // vector_width
     latency = fir_cycles
 
-    super().__init__(name, latency_cycles=latency)
+    super().__init__(name, latency_cycles=latency, is_fixed_point=is_fixed_point)
     
     self.window_size = window_size
     self.vector_width = vector_width
@@ -23,11 +23,14 @@ class DerivativeUnit(HardwareUnit):
     x_current = np.array(data)
     x_extended = np.concatenate([self.x_history, x_current])
     
-    results = (1/8) * (2 * x_extended[4:] + x_extended[3:-1] - x_extended[1:-3] - 2 * x_extended[:self.window_size])
+    if self.is_fixed_point:
+      results = (2 * x_extended[4:] + x_extended[3:-1] - x_extended[1:-3] - 2 * x_extended[:self.window_size])
+      # y[n] = [2x[n] + x[n-1] - x[n-3] - 2x[n-4]] >> 3
+      results = results.astype(np.int32) >> 3
+    else:
+      # y[n] = (1/8) * [2x[n] + x[n-1] - x[n-3] - 2x[n-4]]
+      results = (1/8) * (2 * x_extended[4:] + x_extended[3:-1] - x_extended[1:-3] - 2 * x_extended[:self.window_size])
 
     self.x_history = x_current[-4:]
     
     return results.tolist()
-
-  def __repr__(self) -> str:
-    return f"<DerivativeUnit name={self.name} latency_cycles={self.latency_cycles} busy={self.busy}>"

high_pass_unit.py
@@ -3,7 +3,7 @@ from hardware_unit import HardwareUnit
 
 class HighPassUnit(HardwareUnit):
 
-  def __init__(self, name: str, window_size: int, vector_width: int):
+  def __init__(self, name: str, window_size: int, vector_width: int, is_fixed_point: bool):
 
     # 1. FIR Stage: (-(1/32)x[n] + x[n-16] - x[n-17] + (1/32)x[n-32])
     #    - Exploit DLP: We emulate SIMD vectorization where "vector_width" samples are processed in parallel.
@@ -16,7 +16,7 @@ class HighPassUnit(HardwareUnit):
     iir_cycles = 1 * window_size
     latency = fir_cycles + iir_cycles
 
-    super().__init__(name, latency_cycles=latency)
+    super().__init__(name, latency_cycles=latency, is_fixed_point=is_fixed_point)
     
     self.window_size = window_size
     self.vector_width = vector_width
@@ -30,22 +30,21 @@ class HighPassUnit(HardwareUnit):
     x_current = np.array(data)
     x_extended = np.concatenate([self.x_history, x_current])
     
-    # part_a = -(1/32)x[n] + x[n-16] - x[n-17] + (1/32)x[n-32]
-    part_a = (-(1/32) * x_extended[32:] + x_extended[16:-16] - x_extended[15:-17] + (1/32) * x_extended[:self.window_size])
+    if self.is_fixed_point:
+      x_n = x_extended[32:].astype(np.int32)
+      x_32 = x_extended[:self.window_size].astype(np.int32)
+      # part_a = -(1/32)x[n] + x[n-16] - x[n-17] + (1/32)x[n-32]
+      part_a = -(x_n >> 5) + x_extended[16:-16] - x_extended[15:-17] + (x_32 >> 5)
+    else:
+      # part_a = -(1/32)x[n] + x[n-16] - x[n-17] + (1/32)x[n-32]
+      part_a = (-(1/32) * x_extended[32:] + x_extended[16:-16] - x_extended[15:-17] + (1/32) * x_extended[:self.window_size])
 
     results = []
     for i in range(len(part_a)):
-      
       # y[n] = y[n-1] + part_a
       y_n = self.y_1 + part_a[i]
-      
       self.y_1 = y_n
-      
       results.append(y_n)
 
     self.x_history = x_current[-32:]
-    
     return results
-
-  def __repr__(self) -> str:
-    return f"<HighPassUnit name={self.name} latency_cycles={self.latency_cycles} busy={self.busy}>"

low_pass_unit.py
@@ -2,7 +2,7 @@ import numpy as np
 from hardware_unit import HardwareUnit
 class LowPassUnit(HardwareUnit):
 
-  def __init__(self, name: str, window_size: int, vector_width: int):
+  def __init__(self, name: str, window_size: int, vector_width: int, is_fixed_point: bool):
     # 1. FIR Stage: (x[n] - 2x[n-6] + x[n-12])
     #    - Exploit DLP: We emulate SIMD vectorization where "vector_width" samples are processed in parallel.
     #    - Cost: 2 ops (sub, add) per sample. 2* is a "free" bit-shift.
@@ -11,11 +11,12 @@ class LowPassUnit(HardwareUnit):
     # 2. IIR Stage: (2y[n-1] - y[n-2] + part_a)
     #    - Sequential Bottleneck: y[n] has a dependency on y[n-1] and y[n-2].
     #    - Cost: 2 ops (sub, add) per sample. 2* is a "free" bit-shift.
-    iir_cycles = 2 * window_size
+    # iir_cycles = 2 * window_size
+    iir_cycles = window_size
 
     latency = fir_cycles + iir_cycles
 
-    super().__init__(name, latency_cycles=latency)
+    super().__init__(name, latency_cycles=latency, is_fixed_point=is_fixed_point)
     
     self.vector_width = vector_width
 
@@ -29,23 +30,24 @@ class LowPassUnit(HardwareUnit):
     x_current = np.array(data)
     x_extended = np.concatenate([self.x_history, x_current])
     
-    # part_a = x[n] - 2x[n-6] + x[n-12]
-    part_a = (x_extended[12:] - 2 * x_extended[6:-6] + x_extended[:-12])
+    if self.is_fixed_point:
+      # part_a = x[n] - x[n-6] << 1 + x[n-12]
+      part_a = x_extended[12:] - (x_extended[6:-6].astype(np.int32) << 1) + x_extended[:-12]
+    else:
+      # part_a = x[n] - 2x[n-6] + x[n-12]
+      part_a = (x_extended[12:] - 2 * x_extended[6:-6] + x_extended[:-12])
 
     results = []
     for i in range(len(part_a)):
+      if self.is_fixed_point:
+        # y[n] = y[n-1] << 1 - y[n-2] + part_a
+        y_n = (int(self.y_1) << 1) - int(self.y_2) + int(part_a[i])
+      else:
+        # y[n] = 2y[n-1] - y[n-2] + part_a
+        y_n = (2 * self.y_1) - self.y_2 + part_a[i]
       
-      # y[n] = 2y[n-1] - y[n-2] + part_a
-      y_n = (2 * self.y_1) - self.y_2 + part_a[i]
-      
-      self.y_2 = self.y_1
-      self.y_1 = y_n
-      
+      self.y_2, self.y_1 = self.y_1, y_n
       results.append(y_n)
 
     self.x_history = x_current[-12:]
-    
     return results
-
-  def __repr__(self) -> str:
-    return f"<LowPassUnit name={self.name} latency_cycles={self.latency_cycles} busy={self.busy}>"

setup.py
@@ -14,24 +14,21 @@ import matplotlib.pyplot as plt
 from high_pass_unit import HighPassUnit
 from threshold_unit import ThresholdUnit
 
-# TODO: Confirm that these are correct
-def get_patient_bpm(patient_number):
-  # https://www.researchgate.net/figure/Heart-rates-of-patients-from-the-MIT-BIH-Arrhythmia-Database_tbl4_371898998
-  patient_bpms = {100: 76, 101: 62, 102: 73, 103: 70, 104: 77, 105: 90, 106: 70, 107: 71, 108: 61, 109: 85}
-  return patient_bpms[patient_number]
+def get_patient_bpm(patient_number, data_dir="ecg_data"):
+  record_path = f"{data_dir}/patient_{patient_number}/{patient_number}"
+  record = wfdb.rdrecord(record_path)
+  annotation = wfdb.rdann(record_path, "atr")
+  
+  r_peaks = annotation.sample[np.isin(annotation.symbol, ["N", "L", "R", "B", "A", "a", "J", "S", "V", "F", "e", "j"])]
+  rr_intervals = np.diff(r_peaks) / record.fs
+  median_bpm = round(60 / np.median(rr_intervals))
+  
+  return median_bpm
 
-# TODO: Confirm that this is working correctly
-def load_float_samples(record_path: str, channel: int = 0) -> np.ndarray:
+def load_data(record_path: str, channel: int = 0):
   record = wfdb.rdrecord(record_path)
   raw = record.p_signal[:, channel]
   return raw.astype(np.float32)
-
-# TODO: Confirm that this is working correctly
-def load_fixed_samples(record_path: str, channel: int = 0) -> np.ndarray:
-  record = wfdb.rdrecord(record_path)
-  raw = record.p_signal[:, channel]
-  normalised = raw / np.max(np.abs(raw))
-  return (normalised * FIXED_POINT_SCALE).astype(np.int16)
 
 def plot_data_recorders(recorders: list[DataRecorder], patient_number: int) -> None:
   n = len(recorders)
@@ -46,20 +43,12 @@ def plot_data_recorders(recorders: list[DataRecorder], patient_number: int) -> N
     ax.set_ylabel("Amplitude")
     ax.grid(True, alpha=0.3)
 
-  axes[-1].set_xlabel("Time (s)")
-  plt.suptitle(f"Patient {patient_number}", fontsize=24, y=0.99)
+  axes[-1].set_xlabel("Sample")
+  plt.suptitle(f"Patient {patient_number}", fontsize=24)
   plt.tight_layout()
   plt.show()
 
-if __name__ == "__main__":
-  # patient_number = input("Enter the patient number (100, 101, or 102): ")
-  patient_number = 102
-
-  record_path = f"ecg_data/patient_{patient_number}/{patient_number}"
-
-  float_samples = load_float_samples(record_path)
-  fixed_samples = load_fixed_samples(record_path)
-
+def run_simulation(patient_samples, is_fixed: bool):
   raw_ecg_data_recorder = DataRecorder("raw_ecg_data_recorder", capacity=DATA_RECORDER_CAPACITY, hop_size=HOP_SIZE)
   low_pass_data_recorder = DataRecorder("low_pass_data_recorder", capacity=DATA_RECORDER_CAPACITY, hop_size=HOP_SIZE)
   high_pass_data_recorder = DataRecorder("high_pass_data_recorder", capacity=DATA_RECORDER_CAPACITY, hop_size=HOP_SIZE)
@@ -68,69 +57,75 @@ if __name__ == "__main__":
   mwi_data_recorder = DataRecorder("mwi_data_recorder", capacity=DATA_RECORDER_CAPACITY, hop_size=HOP_SIZE)
   threshold_data_recorder = DataRecorder("threshold_data_recorder", capacity=DATA_RECORDER_CAPACITY, hop_size=HOP_SIZE)
 
-  data_recorders = [raw_ecg_data_recorder, low_pass_data_recorder, high_pass_data_recorder, derivative_data_recorder, squaring_data_recorder, mwi_data_recorder, threshold_data_recorder]
-
-  sample_queue = SampleQueue("sample_queue", queue_size=QUEUE_SIZE, window_size=WINDOW_SIZE, hop_size=HOP_SIZE)
-  data_uploader = DataUploader("data_uploader", float_samples)
+  sample_queue = SampleQueue("sample_queue", QUEUE_SIZE, WINDOW_SIZE, HOP_SIZE)
+  data_uploader = DataUploader("data_uploader", patient_samples)
   data_uploader.connect(sample_queue)
 
-  low_pass_unit = LowPassUnit("low_pass_unit", window_size=WINDOW_SIZE, vector_width=VECTOR_WIDTH)
+  low_pass_unit = LowPassUnit("low_pass_unit", WINDOW_SIZE, VECTOR_WIDTH, is_fixed_point=is_fixed)
   low_pass_unit.attach_recorder(low_pass_data_recorder)
 
-  high_pass_unit = HighPassUnit("high_pass_unit", window_size=WINDOW_SIZE, vector_width=VECTOR_WIDTH)
+  high_pass_unit = HighPassUnit("high_pass_unit", WINDOW_SIZE, VECTOR_WIDTH, is_fixed_point=is_fixed)
   high_pass_unit.attach_recorder(high_pass_data_recorder)
   
-  derivative_unit = DerivativeUnit("derivative_unit", window_size=WINDOW_SIZE, vector_width=VECTOR_WIDTH)
+  derivative_unit = DerivativeUnit("derivative_unit", WINDOW_SIZE, VECTOR_WIDTH, is_fixed_point=is_fixed)
   derivative_unit.attach_recorder(derivative_data_recorder)
-  
-  squaring_unit = SquaringUnit("squaring_unit", window_size=WINDOW_SIZE, vector_width=VECTOR_WIDTH)
+
+  squaring_unit = SquaringUnit("squaring_unit", WINDOW_SIZE, VECTOR_WIDTH, is_fixed_point=is_fixed)
   squaring_unit.attach_recorder(squaring_data_recorder)
 
-  mwi_unit = MWIUnit("mwi_unit", window_size=WINDOW_SIZE, vector_width=VECTOR_WIDTH, mwi_window_size=MWI_WINDOW_SIZE)
+  mwi_unit = MWIUnit("mwi_unit", WINDOW_SIZE, VECTOR_WIDTH, MWI_WINDOW_SIZE, is_fixed_point=is_fixed)
   mwi_unit.attach_recorder(mwi_data_recorder)
 
-  threshold_unit = ThresholdUnit("threshold_unit", window_size=WINDOW_SIZE, sample_rate=SAMPLE_RATE)
+  threshold_unit = ThresholdUnit("threshold_unit", WINDOW_SIZE, SAMPLE_RATE, is_fixed_point=is_fixed)
   threshold_unit.attach_recorder(threshold_data_recorder)
-  
+
   low_pass_unit.connect(high_pass_unit).connect(derivative_unit).connect(squaring_unit).connect(mwi_unit).connect(threshold_unit)
+  
+  scheduler_unit = Scheduler("sch", sample_queue, lanes=[low_pass_unit])
+  scheduler_unit.attach_recorder(raw_ecg_data_recorder)
 
-  scheduler = Scheduler("scheduler", sample_queue, lanes=[low_pass_unit])
-  scheduler.attach_recorder(raw_ecg_data_recorder)
+  units = [sample_queue, data_uploader, scheduler_unit, low_pass_unit, high_pass_unit, derivative_unit, squaring_unit, mwi_unit, threshold_unit]
+  clock = ClockUnit()
+  clock.subscribe_many(units)
 
-  units = [sample_queue, data_uploader, scheduler, low_pass_unit, high_pass_unit, derivative_unit, squaring_unit, mwi_unit, threshold_unit]
-  clock_unit = ClockUnit()
-  clock_unit.subscribe_many(units)
+  while True:
+    clock.tick()
+    if not data_uploader.active and all(not u.busy and not u.output_data for u in units):
+      break
+  
+  return [raw_ecg_data_recorder, low_pass_data_recorder, high_pass_data_recorder, derivative_data_recorder, squaring_data_recorder, mwi_data_recorder, threshold_data_recorder], threshold_unit
 
-  # TODO: Should stop running when all samples are processed
-  for _ in range(1000000):
-    clock_unit.tick()
+if __name__ == "__main__":
+  # patient_number = input("Enter the patient number (116, 123, or 215): ")
+  patient_number = 116
 
-    if clock_unit.cycle % 100000 == 0:
-      print(clock_unit)
+  record_path = f"ecg_data/patient_{patient_number}/{patient_number}"
 
-  plot_data_recorders(data_recorders, patient_number=patient_number)
+  float_samples = load_data(record_path)
+  fixed_samples = (float_samples * FIXED_POINT_SCALE).astype(np.int32)
 
-  print(f"Calculated BPM = {threshold_unit.get_bpm()}")
-  print(f"Actual BPM = {get_patient_bpm(patient_number)}")
+  print("Running Floating Point Simulation...")
+  float_recorders, float_unit = run_simulation(float_samples, is_fixed=False)
+  float_bpm = float_unit.get_bpm()
 
-  print(clock_unit)
-  print(data_uploader)
-  print(sample_queue)
-  print(scheduler)
-  print(low_pass_unit)
-  print(high_pass_unit)
-  print(derivative_unit)
-  print(squaring_unit)
-  print(mwi_unit)
-  print(threshold_unit)
+  print("Running Fixed Point Simulation...")
+  fixed_recorders, fixed_unit = run_simulation(fixed_samples, is_fixed=True)
+  fixed_bpm = fixed_unit.get_bpm()
 
-  # Confirm that the load_float_samples and load_fixed_samples load data as fixed and float.
-  # The latency of the pipeline units (low pass, high pass, derivative, squaring, window, and threshold) should change based on fixed and floating point samples.
-  # Confirm that the threshold unit is valid. It most likely is since we get the right BPM, but look it over nonetheless.
-  # Confirm that the moving window integration unit is valid. It most likely is since we get the right BPM, but look it over nonetheless.
-  # Confirm that each of the pipeline units is using some form of parallelization (if possible).
-  # Remove the lane functionality from the scheduler (we only have 1 lane now).
-  # There is no overlap between windows anymore. AI told me that this was neccessary, but from what I can tell, we are calculating the BPM correctly so I see no point in adding more complexity. Find a reason for this.
-  # Look into how we can analyze throughput vs battery life.
-  # It seems like the data being plotted is slightly offset from other pipeline stages. Confirm if this is ok.
-  # Figure out the issue with the large number of stalled cycles.
+  print(f"\nPatient #{patient_number}:")
+  print(f"Actual BPM (Database): {get_patient_bpm(patient_number)}")
+  print(f"Float Mode BPM: {float_bpm}")
+  print(f"Fixed Mode BPM: {fixed_bpm}")
+
+  print("\nFloat Point vs Fixed Point Results:")
+  for i in range(len(float_recorders)-1):
+    float_signal = np.array(float_recorders[i].get_signal())
+    fixed_signal = np.array(fixed_recorders[i].get_signal()) / FIXED_POINT_SCALE
+
+    min_len = min(len(float_signal), len(fixed_signal))
+    
+    rmse = np.sqrt(np.mean((float_signal[:min_len] - fixed_signal[:min_len])**2))
+    print(f"{float_recorders[i].name} RMSE: {rmse:.8f}")
+
+  plot_data_recorders(fixed_recorders, patient_number=patient_number)
+  plot_data_recorders(float_recorders, patient_number=patient_number)

squaring_unit.py
@@ -1,16 +1,17 @@
 import numpy as np
 from hardware_unit import HardwareUnit
+from config import FIXED_POINT_BITS
 
 class SquaringUnit(HardwareUnit):
 
-  def __init__(self, name: str, window_size: int, vector_width: int):
+  def __init__(self, name: str, window_size: int, vector_width: int, is_fixed_point: bool):
     # 1. FIR Stage: x[n]^2
     #    - Exploit DLP: We emulate SIMD vectorization where "vector_width" samples are processed in parallel.
     #    - Cost: 1 op (mul) per sample.
     fir_cycles = (1 * window_size) // vector_width
     latency = fir_cycles
 
-    super().__init__(name, latency_cycles=latency)
+    super().__init__(name, latency_cycles=latency, is_fixed_point=is_fixed_point)
     
     self.vector_width = vector_width
 
@@ -18,9 +19,12 @@ class SquaringUnit(HardwareUnit):
   def compute(self, data: list) -> list:
     x_current = np.array(data)
     
-    results = np.square(x_current)
+    if self.is_fixed_point:
+      # int64 to prevent overflow
+      # y[n] = x[n]^2 >> 15
+      results = (x_current.astype(np.int64)**2) >> FIXED_POINT_BITS
+    else:
+      # y[n] = x[n]^2
+      results = np.square(x_current)
 
     return results.tolist()
-
-  def __repr__(self) -> str:
-    return f"<SquaringUnit name={self.name} latency_cycles={self.latency_cycles} busy={self.busy}>"